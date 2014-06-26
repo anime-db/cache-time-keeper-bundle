8988f2c9f27ed3bdb2b111e1950b6f5c87ff6de1
Peter Gribanov <info@peter-gribanov.ru>
2014-06-26 23:02:03 +0400

    add bundle

---
+++ src/AnimeDb/Bundle/CacheKeyKeeperBundle/AnimeDbCacheKeyKeeperBundle.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CacheKeyKeeperBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle
+ *
+ * @package AnimeDb\Bundle\CacheKeyKeeperBundle
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class AnimeDbCacheKeyKeeperBundle extends Bundle
+{
+}
