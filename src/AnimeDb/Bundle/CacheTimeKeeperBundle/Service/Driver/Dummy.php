199825eb0e17e8c59f17cbeef181aa7f67b30aa1
Peter Gribanov <info@peter-gribanov.ru>
2014-06-27 19:29:24 +0400

    add dummy driver

---
+++ src/AnimeDb/Bundle/CacheTimeKeeperBundle/Service/Driver/Dummy.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Driver;
+
+use AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Driver;
+
+/**
+ * Dummy driver
+ *
+ * @package AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Driver
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Dummy implements Driver
+{
+    /**
+     * Get time for key
+     *
+     * @param string $key
+     *
+     * @return \DateTime
+     */
+    public function get($key)
+    {
+        return new \DateTime();
+    }
+
+    /**
+     * Set time for key
+     *
+     * @param string $key
+     * @param \DateTime $time
+     *
+     * @return boolean
+     */
+    public function set($key, \DateTime $time)
+    {
+        return true;
+    }
+
+    /**
+     * Get a list of keys or dates and chooses the max date
+     *
+     * @param array $params
+     *
+     * @return \DateTime
+     */
+    public function getMax(array $params)
+    {
+        return new \DateTime();
+    }
+}
