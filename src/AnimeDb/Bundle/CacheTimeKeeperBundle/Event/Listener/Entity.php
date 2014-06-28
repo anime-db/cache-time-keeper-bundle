b3339d3041ef26a8c5111b64bcadbcff5e5c6f87
Peter Gribanov <info@peter-gribanov.ru>
2014-06-28 13:07:44 +0400

    add entity listener

---
+++ src/AnimeDb/Bundle/CacheTimeKeeperBundle/Event/Listener/Entity.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener;
+
+use AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+/**
+ * Entity listener
+ *
+ * @package AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Entity
+{
+    /**
+     * Keeper
+     *
+     * @var \AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper
+     */
+    protected $keeper;
+
+    /**
+     * Construct
+     *
+     * @param \AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper $keeper
+     */
+    public function __construct(Keeper $keeper)
+    {
+        $this->keeper = $keeper;
+    }
+
+    /**
+     * Post persist
+     *
+     * @param \Doctrine\ORM\Event\LifecycleEventArgs $args
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $this->keeper->set($this->getKeyFromEntity($args), new \DateTime());
+    }
+
+    /**
+     * Post remove
+     *
+     * @param \Doctrine\ORM\Event\LifecycleEventArgs $args
+     */
+    public function postRemove(LifecycleEventArgs $args)
+    {
+        $this->keeper->set($this->getKeyFromEntity($args), new \DateTime());
+    }
+
+    /**
+     * Post update
+     *
+     * @param \Doctrine\ORM\Event\LifecycleEventArgs $args
+     */
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $this->keeper->set($this->getKeyFromEntity($args), new \DateTime());
+    }
+
+    /**
+     * Get key from entity
+     *
+     * @param \Doctrine\ORM\Event\LifecycleEventArgs $args
+     *
+     * @return string
+     */
+    protected function getKeyFromEntity(LifecycleEventArgs $args)
+    {
+        return $args->getEntityManager()->getClassMetadata(get_class($args->getEntity()))->getName();
+    }
+}
