24b7d5289c6dfde4940e380c5c15d7c0c4c15cd2
Peter Gribanov <info@peter-gribanov.ru>
2014-06-28 19:15:28 +0400

    add event listener entity test

---
+++ src/AnimeDb/Bundle/CacheTimeKeeperBundle/Tests/Event/Listener/EntityTest.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+namespace AnimeDb\Bundle\CacheTimeKeeperBundle\Test\Event\Listener;
+
+use AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity;
+
+/**
+ * Test entity event listener
+ *
+ * @package AnimeDb\Bundle\CacheTimeKeeperBundle\Test\Event\Listener
+ * @author Peter Gribanov <info@peter-gribanov.ru>
+ */
+class EntityTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test post persist
+     *
+     * @covers \AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity::__construct
+     * @covers \AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity::postPersist
+     * @covers \AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity::getKeyFromEntity
+     */
+    public function testPostPersist()
+    {
+        $keeper_mock = $this
+            ->getMockBuilder('\AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $keeper_mock
+            ->expects($this->once())
+            ->method('set')
+            ->with('foo', new \DateTime());
+
+        $obj = new Entity($keeper_mock);
+        $obj->postPersist($this->getEventMock());
+    }
+
+    /**
+     * Test post remove
+     *
+     * @covers \AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity::__construct
+     * @covers \AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity::postRemove
+     * @covers \AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity::getKeyFromEntity
+     */
+    public function testPostRemove()
+    {
+        $keeper_mock = $this
+            ->getMockBuilder('\AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $keeper_mock
+            ->expects($this->once())
+            ->method('set')
+            ->with('foo', new \DateTime());
+
+        $obj = new Entity($keeper_mock);
+        $obj->postRemove($this->getEventMock());
+    }
+
+    /**
+     * Test post update
+     *
+     * @covers \AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity::__construct
+     * @covers \AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity::postUpdate
+     * @covers \AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\Entity::getKeyFromEntity
+     */
+    public function testPostUpdate()
+    {
+        $keeper_mock = $this
+            ->getMockBuilder('\AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $keeper_mock
+            ->expects($this->once())
+            ->method('set')
+            ->with('foo', new \DateTime());
+
+        $obj = new Entity($keeper_mock);
+        $obj->postUpdate($this->getEventMock());
+    }
+
+    /**
+     * Get event mock
+     *
+     * @return \Doctrine\ORM\Event\LifecycleEventArgs
+     */
+    protected function getEventMock()
+    {
+        $meta_mock = $this
+            ->getMockBuilder('\Doctrine\ORM\Mapping\ClassMetadata')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $meta_mock
+            ->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue('foo'));
+
+        $em_mock = $this
+            ->getMockBuilder('\Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $em_mock
+            ->expects($this->once())
+            ->method('getClassMetadata')
+            ->with('stdClass')
+            ->will($this->returnValue($meta_mock));
+
+        $args_mock = $this
+            ->getMockBuilder('\Doctrine\ORM\Event\LifecycleEventArgs')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $args_mock
+            ->expects($this->once())
+            ->method('getEntityManager')
+            ->will($this->returnValue($em_mock));
+        $args_mock
+            ->expects($this->once())
+            ->method('getEntity')
+            ->will($this->returnValue(new \stdClass()));
+        return $args_mock;
+    }
+}
