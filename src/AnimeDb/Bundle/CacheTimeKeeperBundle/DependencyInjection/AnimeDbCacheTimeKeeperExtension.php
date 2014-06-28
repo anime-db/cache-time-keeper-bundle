3fe499cf497ba201c0eda29fd5307d3a9f16501d
Peter Gribanov <info@peter-gribanov.ru>
2014-06-28 14:31:39 +0400

    change require

--- src/AnimeDb/Bundle/CacheTimeKeeperBundle/DependencyInjection/AnimeDbCacheTimeKeeperExtension.php
+++ src/AnimeDb/Bundle/CacheTimeKeeperBundle/DependencyInjection/AnimeDbCacheTimeKeeperExtension.php
@@ -5,7 +5,7 @@ namespace AnimeDb\Bundle\CacheTimeKeeperBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -19,7 +19,7 @@ class AnimeDbCacheTimeKeeperExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('parameters.yml');
         $loader->load('services.yml');
 
