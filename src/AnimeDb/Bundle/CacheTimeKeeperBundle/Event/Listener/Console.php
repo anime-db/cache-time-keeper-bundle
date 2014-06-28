89d18001359cc44e64d9358120ce44240f013283
Peter Gribanov <info@peter-gribanov.ru>
2014-06-28 13:14:13 +0400

    add console listener

---
+++ src/AnimeDb/Bundle/CacheTimeKeeperBundle/Event/Listener/Console.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener;
+
+use AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+
+/**
+ * Console listener
+ *
+ * @package AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Console
+{
+    /**
+     * Keeper
+     *
+     * @var \AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper
+     */
+    protected $keeper;
+
+    /**
+     * Construct
+     *
+     * @param \AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper $keeper
+     */
+    public function __construct(Keeper $keeper)
+    {
+        $this->keeper = $keeper;
+    }
+
+    /**
+     * On terminate command
+     *
+     * @param \Symfony\Component\Console\Event\ConsoleTerminateEvent $event
+     */
+    public function onTerminate(ConsoleTerminateEvent $event)
+    {
+        if ($event->getCommand()->getName() == 'cache:clear') {
+            $this->keeper->set(Keeper::LAST_UPDATE_KEY, new \DateTime());
+        }
+    }
+}
