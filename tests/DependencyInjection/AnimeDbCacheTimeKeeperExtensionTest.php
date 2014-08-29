bcc9c9b5be2b235993cbc4d1656850505f96a90e
Peter Gribanov <info@peter-gribanov.ru>
2014-08-29 17:33:45 +0400

    add test for di

---
+++ tests/DependencyInjection/AnimeDbCacheTimeKeeperExtensionTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CacheTimeKeeperBundle\Tests\DependencyInjection;
+
+use AnimeDb\Bundle\CacheTimeKeeperBundle\DependencyInjection\AnimeDbCacheTimeKeeperExtension;
+
+/**
+ * Test DependencyInjection
+ *
+ * @package AnimeDb\Bundle\AniDbBrowserBundle\Tests\DependencyInjection
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class AnimeDbCacheTimeKeeperExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test load
+     */
+    public function testLoad()
+    {
+        $di = new AnimeDbCacheTimeKeeperExtension();
+        $di->load([], $this->getMock('Symfony\Component\DependencyInjection\ContainerBuilder'));
+    }
+}
