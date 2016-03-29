e4f082bf04a2cf74c70a04902f9d03f0e369f408
Peter Gribanov <wcode404@gmail.com>
2016-03-29 14:33:28 +0300

    create base TestCase for get mock disable original constructor

---
+++ tests/TestCase.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2014, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace AnimeDb\Bundle\CacheTimeKeeperBundle\Tests;
+
+
+/**
+ * @package AnimeDb\Bundle\CacheTimeKeeperBundle\Tests\Event\Listener
+ * @author Peter Gribanov <info@peter-gribanov.ru>
+ */
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param string $original_class_name
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    public function getMockClass($original_class_name)
+    {
+        return $this
+            ->getMockBuilder($original_class_name)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+}
