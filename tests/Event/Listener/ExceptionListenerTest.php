952e1ff6f70aefd8866b45229fb83ed7b26cc11c
Peter Gribanov <wcode404@gmail.com>
2016-06-06 17:16:11 +0300

    test ExceptionListener

---
+++ tests/Event/Listener/ExceptionListenerTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * AnimeDb package.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2014, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace AnimeDb\Bundle\CacheTimeKeeperBundle\Tests\Event\Listener;
+
+use AnimeDb\Bundle\CacheTimeKeeperBundle\Event\Listener\ExceptionListener;
+use AnimeDb\Bundle\CacheTimeKeeperBundle\Exception\NotModifiedException;
+use AnimeDb\Bundle\CacheTimeKeeperBundle\Tests\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class ExceptionListenerTest extends TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|GetResponseForExceptionEvent
+     */
+    protected $event;
+
+    /**
+     * @var ExceptionListener
+     */
+    protected $listener;
+
+    protected function setUp()
+    {
+        $this->event = $this->getMockObject(GetResponseForExceptionEvent::class);
+        $this->listener = new ExceptionListener();
+    }
+
+    public function testOnKernelExceptionIgnore()
+    {
+        $this->event
+            ->expects($this->once())
+            ->method('getException')
+            ->will($this->returnValue(new \RuntimeException()));
+
+        $this->event
+            ->expects($this->never())
+            ->method('setResponse');
+
+        $this->listener->onKernelException($this->event);
+    }
+
+    public function testOnKernelException()
+    {
+        $response = $this->getMock(Response::class);
+
+        $exception = $this->getMockObject(NotModifiedException::class);
+        $exception
+            ->expects($this->once())
+            ->method('getResponse')
+            ->will($this->returnValue($response));
+
+        $this->event
+            ->expects($this->once())
+            ->method('getException')
+            ->will($this->returnValue($exception));
+
+        $this->event
+            ->expects($this->once())
+            ->method('setResponse')
+            ->with($response);
+
+        $this->listener->onKernelException($this->event);
+    }
+}
