ee73ce7785cab13f1a20bb56f8054d93cd40ce9e
Peter Gribanov <wcode404@gmail.com>
2016-06-07 16:47:42 +0300

    Add command for reset last update date of the project

---
+++ src/Command/ResetCommand.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * AnimeDb package.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2014, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace AnimeDb\Bundle\CacheTimeKeeperBundle\Command;
+
+use AnimeDb\Bundle\CacheTimeKeeperBundle\Service\Keeper;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ResetCommand extends Command
+{
+    /**
+     * @var Keeper
+     */
+    protected $keeper;
+
+    /**
+     * @param Keeper $keeper
+     */
+    public function __construct(Keeper $keeper)
+    {
+        $this->keeper = $keeper;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('cache:reset-cache-time-keeper')
+            ->setDescription('Reset last update date of the project')
+            ->setHelp(<<<HELP
+Command only reset date of update. Not remove data from the storage. 
+HELP
+);
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return integer|null
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->keeper->set(Keeper::LAST_UPDATE_KEY, new \DateTime());
+        $output->writeln('Reset last update date of the project is complete.');
+    }
+}
